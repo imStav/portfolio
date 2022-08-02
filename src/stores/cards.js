8b60db9885ed236685ed597277b1468967e0748b
imStav <saprocontact@gmail.com>
2022-08-02 18:57:30 +0200

    Updating preview pictures

--- src/stores/cards.js
+++ src/stores/cards.js
@@ -7,7 +7,7 @@ export const cardStore = defineStore({
       pokedesk: {
         name: "Pokedesk",
         preview:
-          "https://raw.githubusercontent.com/imStav/pokedesk/main/src/assets/screenshots/pokedesk_dark_preview.png",
+          "/assets/screenshots/pokedesk_dark_preview.png",
         description:
           "A fictional desktop session of the Pokémon's Pr Chen. Imitating a Windows 11 like user interface, the project has been made using Vite, Vue.js, Pinia and Tailwind CSS.",
         appLink: "",
@@ -18,7 +18,7 @@ export const cardStore = defineStore({
       rickandmorty: {
         name: "Tracking Rick & Morty",
         preview:
-          "https://raw.githubusercontent.com/imStav/tracking-rickandmorty/main/src/assets/screenshots/tracking-rickandmorty_preview.png",
+          "/assets/screenshots/tracking-rickandmorty_preview.png",
         description:
           "Tracking Rick & Morty is a minimalistic app that aims to track the characters statuses of the TV show Rick & Morty.",
         appLink: "tracking-rickandmorty.netlify.app",
@@ -29,7 +29,7 @@ export const cardStore = defineStore({
       abstract: {
         name: "Abstract",
         preview:
-          "https://raw.githubusercontent.com/imStav/abstract/main/src/assets/screenshots/abstract_dark_preview.JPG",
+          "/assets/screenshots/abstract_light_preview.png",
         description:
           "Abtract is a small web app that provides free wallpapers i've made with Figma. This new version is based on Vue 3 and Tailnd CSS.",
         prefix: "https://",
@@ -40,7 +40,7 @@ export const cardStore = defineStore({
       dbz: {
         name: "DBZ JS",
         preview:
-          "https://raw.githubusercontent.com/imStav/dbz-js/main/screenshots/dbz_js_preview.JPG",
+          "/assets/screenshots/dbz-js_preview.png",
         description:
           "DBZ JS for 'Dragon Ball FighterZ JavaScript', is a simple JS program that takes 2 random fighters, launches a brawl and return the winner by an alert.",
         prefix: "",
@@ -51,7 +51,7 @@ export const cardStore = defineStore({
       undefined: {
         name: "Pop School undefined",
         preview:
-          "https://raw.githubusercontent.com/imStav/popschool-undefined/main/assets/pop-undefined_preview.png",
+          "/assets/screenshots/pop-undefined_preview.png",
         description:
           "A little Vanilla JavaScript project that just aims to introduce my current Pop School class composition. Made using a dedicated API. #undefined",
         prefix: "https://",
