bd7bcaa785f5f204894bafc81be6aa55fb209ecf
imStav <saprocontact@gmail.com>
2022-11-21 21:33:08 +0100

    Added new skills to the About me page

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="about-text">
     <p class="mb-4">
-      Hi there! My name is Steven Averlant (or Stav on the Internet). I am 25
+      Hi there! My name is Steven Averlant (or Stav on the Internet). I am 26
       years old and currently living in France. I've learned front-end web
       development mostly by myself and followed an IT cursus at Pop School then
       (a web developer "school" located in the North of the France).
     </p>
 
     <p>
-      Passionate by web design (UI / UX) and front-end development, i discovered
-      technologies and trends i am actually trying to master as much as i can.
-      Thus, i am always trying to keep in touch with tech news to stay informed.
+      Passionate by web design (UI / UX) and front-end development, I discovered
+      technologies and trends I am actually trying to master as much as I can.
+      Thus, I am always trying to keep in touch with tech news to stay informed.
     </p>
   </div>
 
   <div class="about-text">
     <p>
-      To sum it up, i am working with modern technologies like:
+      To sum it up, I am working with modern technologies like:
 
       <code>
         <ul class="text-gradient my-4 w-fit">
-          <li>Vue.js (v3)</li>
+          <li>Vue 3</li>
+          <li>Nuxt 3</li>
           <li>Vite</li>
           <li>Pinia</li>
-          <li>Vanilla JavaScript</li>
+          <li>JavaScript</li>
           <li>TypeScript (currently learning)</li>
+          <li>Cypress (currently learning)</li>
           <li>Tailwind CSS</li>
           <li>SCSS / SASS</li>
           <li>Git & Github</li>
+          <li>Netlify</li>
         </ul>
       </code>
 
-      Also i can work a bit with:
+      Also I can work a bit with:
 
       <code>
         <ul class="text-gradient my-4 w-fit">
